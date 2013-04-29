48e50af12ddec611407642b8899c855c3a1e5c02
GhozyLab <GhozyLab@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-29 23:38:21 +0000

    Update uninstall.php
    git-svn-id: https://plugins.svn.wordpress.org/easy-media-gallery@705664 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tags/1.1.7/uninstall.php
+++ tags/1.1.7/uninstall.php
@@ -5,12 +5,12 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) )
 // Remove plugin options from database.
 function spg_clean_data() {
 	
-	delete_option( 'easy_media_opt' );
-	
 	    $easymedia_values = get_option( 'easy_media_opt' );
 		if ( is_array( $easymedia_values ) && array_key_exists( $name, $easymedia_values ) ) $keepornot = $easymedia_values['easymedia_disen_databk'];
 	
 	if ( $keepornot != '1' ) {	
+	
+	delete_option( 'easy_media_opt' );
 
 // Remove plugin-specific custom post type entries.	
 		$posts = get_posts( array(
