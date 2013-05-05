118884fe786b4bbc5e54912f6e1900f6d0c1fe05
GhozyLab <GhozyLab@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-05 07:14:00 +0000

    UPDATE: Price
    git-svn-id: https://plugins.svn.wordpress.org/easy-media-gallery/tags/1.1.9@708105 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/settings.php
+++ includes/settings.php
@@ -468,7 +468,7 @@ $i++;
 ?>
  
 <input type="hidden" name="action" value="save" />
-<p><a target="_blank" href="http://ghozylab.com/order" class="tsc_buttons2 red">Upgrade to Pro Version  &nbsp;for only $18</a> <span style="color:#666666;margin-left:2px; font-size:11px;">&nbsp; Need More Features? Upgrade to Pro Version!</span></p>
+<p><a target="_blank" href="http://ghozylab.com/order" class="tsc_buttons2 red">Upgrade to Pro Version  &nbsp;for only $<?php echo EASYMEDIA_PRICE; ?></a> <span style="color:#666666;margin-left:2px; font-size:11px;">&nbsp; Need More Features? Upgrade to Pro Version!</span></p>
  </div> </div>
  </form>
  
