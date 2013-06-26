ac4182e05b00eb4670d267e9eeebd0bb354ecf07
GhozyLab <GhozyLab@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-26 00:35:42 +0000

    Change WordPress Pointers String
    git-svn-id: https://plugins.svn.wordpress.org/easy-media-gallery/trunk@731807 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/functions/functions.php
+++ includes/functions/functions.php
@@ -826,7 +826,7 @@ function easmedia_pointer_pointer_header() {
 }
 
 function easmedia_pointer_pointer_footer() {
-    $pointer_content = '<h3>Congratulations!</h3>';
+    $pointer_content = '<h3>Thank You!</h3>';
 	  $pointer_content .= '<p>You&#39;ve just installed Easy Media Gallery Lite Version. Click <a class="close"href="edit.php?post_type=easymediagallery&page=docs">here</a> to watch video tutorials and user guide plugin.</p>';
 ?>
 
