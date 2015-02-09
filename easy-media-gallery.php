34eb19be248dccdc26037de8078907af9009a163
GhozyLab <GhozyLab@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-09 11:08:43 +0000

    update plugin desc
    git-svn-id: https://plugins.svn.wordpress.org/easy-media-gallery/tags/1.2.95@1085450 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- easy-media-gallery.php
+++ easy-media-gallery.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Easy Media Gallery
 Plugin URI: http://www.ghozylab.com/plugins/
-Description: Easy Media Gallery (Lite) - Displaying your images, videos (MP4, Youtube, Vimeo) and audio mp3 in elegant and fancy lightbox with very easy. Allows you to customize all media to get it looking exactly what you want. <a href="http://ghozylab.com/plugins/easy-media-gallery-pro/pricing/" target="_blank"><strong> Upgrade to Pro Version Now</strong></a> and get a tons of awesome features.
+Description: Easy Media Gallery (Lite) - Displaying your image, video (MP4, Youtube, Vimeo) and audio mp3 in elegant and fancy lightbox with very easy. Allows you to customize all media to get it looking exactly what you want. <a href="http://ghozylab.com/plugins/easy-media-gallery-pro/pricing/" target="_blank"><strong> Upgrade to Pro Version Now</strong></a> and get a tons of awesome features.
 Author: GhozyLab, Inc.
 Version: 1.2.95
 Author URI: http://www.ghozylab.com/plugins/
